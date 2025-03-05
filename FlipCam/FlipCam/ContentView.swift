7a19e7dd2c75bdf376c2ba456107299bf34e6a23
Astrid <84257063+Astrid-sudo@users.noreply.github.com>
2025-03-06 07:03:49 +0800

    [chore](jira #2ptPljlI): Initial commit.

---
+++ FlipCam/FlipCam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlipCam
+//
+//  Created by Astrid Lin on 2025/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlipCam/FlipCam/FlipCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipCamApp.swift
+//  FlipCam
+//
+//  Created by Astrid Lin on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct FlipCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
