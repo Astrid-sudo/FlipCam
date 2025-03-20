e9e71b3a01baf5f4fb22b895b8235a0a71537786
AstridLin <astridlin@kkbox.com>
2025-03-20 08:12:31 +0800

    [feat](trello #LVplIWJi): Make sure camera preview, guide photo and grid guide overlap perfectly.

--- FlipCam/FlipCam/ShotView.swift
+++ FlipCam/FlipCam/ShotView.swift
@@ -13,20 +13,24 @@ struct ShotView: View {
 	var body: some View {
 		VStack {
 			PreviewContainer(camera: camera) {
-				CameraPreview(source: camera.previewSource, camera: camera)
-					.task {
-						await camera.start()
-					}
-					.overlay {
-						if camera.shouldShowGuidePhoto {
-							guidePhoto
+				ZStack {
+					CameraPreview(source: camera.previewSource, camera: camera)
+						.task {
+							await camera.start()
 						}
-					}
-					.overlay {
-						if camera.isGuideGridEnabled {
-							gridLines
+						.aspectRatio(3/4, contentMode: .fit)
+						.overlay(alignment: .center) {
+							GeometryReader { geometry in
+								if camera.shouldShowGuidePhoto {
+									guidePhoto(width: geometry.size.width, height: geometry.size.height)
+								}
+								if camera.isGuideGridEnabled {
+									gridLines
+										.frame(width: geometry.size.width, height: geometry.size.height)
+								}
+							}
 						}
-					}
+				}
 			}
 			GuideControl(camera: camera)
 			OpacityControl(camera: camera)
@@ -35,12 +39,12 @@ struct ShotView: View {
 	}
 
 	@ViewBuilder
-	private var guidePhoto: some View {
+	private func guidePhoto(width: CGFloat, height: CGFloat) -> some View {
 		if let processedGuidePhoto = camera.processedGuidePhoto {
 			Image(uiImage: processedGuidePhoto)
 				.resizable()
 				.aspectRatio(contentMode: .fill)
-				.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width * 4/3, alignment: .center)
+				.frame(width: width, height: height)
 				.clipped()
 				.opacity(camera.guidePhotoOpacity)
 		}
@@ -50,9 +54,7 @@ struct ShotView: View {
 	private var gridLines: some View {
 		GridLines()
 			.stroke(Color.yellow.opacity(0.7), lineWidth: 1.0)
-			.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width * 4/3)
 			.allowsHitTesting(false)
 	}
-
 
 }
